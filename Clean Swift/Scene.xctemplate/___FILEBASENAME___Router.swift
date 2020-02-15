92026c2a17367aefc0ff1d6eeca446d6f04888cc
Darius Vilcinskis <darv@danskebank.lt>
2020-02-15 08:33:16 +0200

    Remove NSObject from router

--- Clean Swift/Scene.xctemplate/___FILEBASENAME___Router.swift
+++ Clean Swift/Scene.xctemplate/___FILEBASENAME___Router.swift
@@ -2,7 +2,7 @@ import UIKit
 
 protocol ___VARIABLE_sceneName___Routing {}
 
-final class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___Routing {
+final class ___VARIABLE_sceneName___Router: ___VARIABLE_sceneName___Routing {
 
     weak var viewController: ___VARIABLE_sceneName___ViewController?
 }
